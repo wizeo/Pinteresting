d18415b65cb7f461995b28ce3876e0504f141a18
Avishai Wizeo <avishai@wizeo.org>
2014-11-16 09:50:37 +0200

    home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "page#home"
+  get "about" => "page#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
